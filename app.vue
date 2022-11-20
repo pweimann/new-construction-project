cc0787eb9071274e96f7119bee317eee5dcfdf67
paul.weimann <paul.weimann@ista-express.de>
2022-11-20 19:11:28 +0100

    implement an initial interactive image test (create a svg file with adobe illustrator and build layers on top where an interactive area is)

--- app.vue
+++ app.vue
@@ -1,4 +1,20 @@
 <template>
   <div>
+    <h1>Working Title</h1>
+    <ImageMap></ImageMap>
   </div>
 </template>
+<script setup lang='ts'>
+</script>
+<style lang='css'>
+
+.construction-image {
+  max-width: 100%;
+}
+
+.overlay {
+  border: red 4px dotted;
+  background-color: red;
+  z-index: 10;
+}
+</style>
