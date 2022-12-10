1ad9497f346b8164192f5bbbbd08d2f8463eec41
paul.weimann <paul.weimann@ista-express.de>
2022-12-10 18:16:25 +0100

    Configure google fonts module and load some fonts.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  modules: [
+    '@nuxtjs/google-fonts'
+  ],
+  googleFonts: {
+    families: {
+      Roboto: true,
+      Geramond: true,
+      Futura: true
+    }
+  }
 })
