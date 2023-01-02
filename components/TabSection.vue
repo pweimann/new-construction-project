0233d4d15a1e9b6cd470264e7925b02bafe72dbf
paul.weimann <paul.weimann@ista-express.de>
2023-01-02 21:10:54 +0100

    Implement the reactivity of the active tab nav item.

--- components/TabSection.vue
+++ components/TabSection.vue
@@ -7,7 +7,10 @@
     <div class='tabs-wrapper full-width'>
       <div class='tabs-nav w-1/3'>
         <ul>
-          <li v-for="navItem in tabsNavList">{{navItem.title}}</li>
+          <li v-for='navItem in tabsNavList' @click='clickTabNavItem(navItem)' :class='activeTabNavItemStyles(navItem)'
+              class='tabs-nav-item text-3xl text-dark-rosa my-2 ml-8'><span
+            class='mr-2'>+</span>{{ navItem.title }}
+          </li>
         </ul>
       </div>
       <div class='tabs-content w-2/3'>
@@ -35,10 +38,42 @@ const tabsNavList: Ref<TabNavItem[]> = ref([
     imageSrc: '/placeholder.jpg'
   }
 ])
+
+const activeNavItem: Ref<TabNavItem | null> = ref(tabsNavList.value[0])
+
+const clickTabNavItem = (item: TabNavItem) => {
+  activeNavItem.value = item
+}
+
+const activeTabNavItemStyles = (item: TabNavItem) => {
+  return item === activeNavItem.value ? 'tab-nav-item--active' : ''
+}
 </script>
-<style lang='css'>
+<style lang='css' scoped>
 .tabs-wrapper {
   display: flex;
   flex-direction: row;
+}
+
+.tabs-nav {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+}
+
+.tabs-nav-item {
+  cursor: pointer;
+  text-decoration: underline;
+  transition: all 2s ease-in-out;
+}
+
+.tabs-nav-item:hover {
+  text-decoration: none;
+}
+
+/*noinspection CssUnusedSymbol*/
+.tab-nav-item--active {
+  text-decoration: none;
 }
 </style>
