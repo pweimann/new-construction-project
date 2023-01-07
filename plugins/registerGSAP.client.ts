f1101ef4d25475712f99d2b114bd39576909e50b
paul.weimann <paul.weimann@ista-express.de>
2023-01-07 18:07:14 +0100

    remove unnecessary comment

--- plugins/registerGSAP.client.ts
+++ plugins/registerGSAP.client.ts
@@ -3,6 +3,5 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import { ScrollToPlugin } from 'gsap/ScrollToPlugin'
 
 export default defineNuxtPlugin(nuxtApp => {
-  // Doing something with nuxtApp
   gsap.registerPlugin(ScrollTrigger, ScrollToPlugin)
 })
