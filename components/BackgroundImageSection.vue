ed6bd7a760d0bd38156a9171882322ad1d4aa17d
paul.weimann <paul.weimann@ista-express.de>
2023-01-08 17:34:17 +0100

    implement a background image parallax effect

--- components/BackgroundImageSection.vue
+++ components/BackgroundImageSection.vue
@@ -1,17 +1,51 @@
 <template>
-  <section>
-    <slot/>
+  <section ref='main'>
+    <div class='parallax-effect'></div>
   </section>
 </template>
 
 <script setup lang='ts'>
+import { onMounted, onUnmounted, ref, defineProps } from 'vue'
+import gsap from 'gsap'
+
+const main = ref()
+const ctx = ref()
+
+onMounted(() => {
+  ctx.value = gsap.context((self) => {
+    if (!self.selector) {
+      console.warn(`gsap selector is undefined -> couldn't create the parallax effect`)
+      return
+    }
+    const imgContainer = self.selector('.parallax-effect')
+    gsap.to(imgContainer, {
+      backgroundPosition: '50% 100%',
+      scrollTrigger: {
+        trigger: imgContainer,
+        start: 'top bottom',
+        end: 'bottom top',
+        scrub: true,
+      }
+    })
+  }, main.value)
+})
+
+onUnmounted(() => {
+  ctx.value.revert()
+})
 </script>
-<style lang='css'>
-img {
-  max-height: 70vh;
-  width: 100%;
-  object-fit: cover;
-  background-size: cover;
+<style lang='css' scoped>
+section {
+  position: relative;
+  min-height: 70vh;
+}
+
+.parallax-effect {
+  position: absolute;
+  background-image: url("@/public/placeholder.jpg");
   background-repeat: no-repeat;
+  background-size: cover;
+  height: 100%;
+  width: 100%;
 }
 </style>
