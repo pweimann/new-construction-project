c237161f7b953e0de9274cf2031930f8bb5d9ed7
paul.weimann <paul.weimann@ista-express.de>
2023-01-28 21:23:23 +0100

    upgrade nuxt and add nuxt image dependency

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
+    '@nuxt/image-edge',
     '@nuxtjs/google-fonts',
     '@nuxtjs/tailwindcss'
   ],
