7472f5eb880c90f4e9799c8f3ae543044bab167d
paul.weimann <paul.weimann@ista-express.de>
2023-02-05 12:57:17 +0100

    fix import path

--- components/BackgroundImageSection.vue
+++ components/BackgroundImageSection.vue
@@ -5,7 +5,7 @@
 </template>
 <style lang='css' scoped>
 .bg-image {
-  background-image: url("@/public/placeholder.jpg");
+  background-image: url("/placeholder.jpg");
   background-repeat: no-repeat;
   background-size: cover;
   min-height: 70vh;
