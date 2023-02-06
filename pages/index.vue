d9b1b49fe52974b77f009cbadab2fbfc72860dbe
paul.weimann <paul.weimann@ista-express.de>
2023-02-06 20:15:08 +0100

    changes ...

--- pages/index.vue
+++ pages/index.vue
@@ -55,7 +55,7 @@ const tabsNavList: Ref<TabNavItem[]> = ref([
   {
     title: 'Mobilität',
     imageSrc: '/tabs/Mobilität_neu.png',
-    description: 'Lorem'
+    description: 'Durch eine Tiefgarage mit abgetrennten Car-Sharing Plätzen und integrierten E-Ladesäulen wird ein großer Beitrag zur Mobilitätswende geleistet. Der im Erdgeschoss verortete, abschließbare Fahrradraum mit Umkleiden und Duschen unterstützt die Wahl des Verkehrsmittels hin zum Fahrrad oder E-Bike und ermöglicht sichere und trockene Parkflächen hierfür. '
   }
 ])
 
@@ -63,40 +63,17 @@ const saveOurPlanetList: Ref<TabNavItem[]> = ref([
   {
     title: 'Save',
     imageSrc: '/tabs/Recycle.png',
-    description: 'Wohnen: Da der Trend des Wohnens hin zu unterschiedlichsten Wohnkonzepten wie auch dem Anstieg der Remote-Arbeiten deutet, wurden durch flexible Wohnungsgrößen von 20m2 bis 120m2 in den Kategorien XS - S - M - L - XL sowie der Grundrissvariabilität innerhalb der jeweiligen Wohnungen selbst (Anpassung einer Raumstruktur an die Bedürfnisse der Bewohnenden) die Adaption des Wohnraums an den jeweiligen Nutzer und Lebensabschnitt sichergestellt.\n'
+    description: ''
   },
   {
     title: 'Our',
     imageSrc: '/tabs/ReUse.png',
-    description: 'Lorem'
+    description: ''
   },
   {
     title: 'Planet',
     imageSrc: '/tabs/Reduce_neu.png',
-    description: 'Das Arbeiten von Morgen wird sowohl von dem Anstieg verschiedenster Technologien wie beispielsweise KI und Automatisierung als auch von Remote-Arbeit und flexiblen Arbeitszeitmodellen geprägt sein. Erhöhte Aufmerksamkeit gilt der Nachhaltigkeit sowie der sozialen Verantwortung von Unternehmen, um ein Beitrag zur Verbesserung der Arbeitsbedingungen für Mitarbeiter und Verringerung der Umweltbelastung zu erzielen.'
-  }
-])
-
-const imageCarousel: Ref<{ src: string, alt?: string }[]> = ref([
-  {
-    src: '/pikto/pikto_1.png',
-    alt: ''
-  },
-  {
-    src: '/pikto/pikto_2.png',
-    alt: ''
-  },
-  {
-    src: '/pikto/pikto_3.png',
-    alt: ''
-  },
-  {
-    src: '/pikto/pikto_4.png',
-    alt: ''
-  },
-  {
-    src: '/pikto/pikto_5.png',
-    alt: ''
+    description: ''
   }
 ])
 </script>
