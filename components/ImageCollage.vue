93aafae70a03305c1c11b3d6b6bfbd5a787c5965
paul.weimann <paul.weimann@ista-express.de>
2023-02-06 21:09:15 +0100

    add remaining renderings

--- components/ImageCollage.vue
+++ components/ImageCollage.vue
@@ -11,16 +11,22 @@
       </div>
       <div class='collage-container mt-8 md:mt-16 px-4 w-full'>
         <div class='collage-left-side'>
-          <img src='/renderings/Persp1.jpg' alt='Image 1'
+          <img src='/renderings/Persp1.jpg' alt='Rendering 1'
                class='collage-left-side-item rounded-3xl object-cover collage-left-side-first'>
-          <img src='/renderings/Persp2.jpg' alt='Image 1'
+          <img src='/renderings/Persp2.jpg' alt='Rendering 2'
                class='collage-left-side-item-above sm:w-1/6 rounded-3xl object-cover collage-left-side-second'>
         </div>
         <div class='collage-right-side'>
-          <img src='/renderings/Persp3.jpg' alt='Image 1'
+          <img src='/renderings/Persp3.jpg' alt='Rendering 3'
                class='collage-right-side-item rounded-3xl object-cover'>
-          <img src='/renderings/Persp4.jpg' alt='Image 1'
+          <img src='/renderings/Persp4.jpg' alt='Rendering 4'
                class='collage-right-side-item-above md:w-1/6 rounded-3xl object-cover'>
+        </div>
+        <div class='collage-left-side'>
+          <img src='/renderings/persp6.jpg' alt='Rendering 5'
+               class='collage-left-side-item rounded-3xl object-cover collage-left-side-first'>
+          <img src='/renderings/persp7.jpg' alt='Rendering 6'
+               class='collage-left-side-item-above sm:w-1/6 rounded-3xl object-cover collage-left-side-second'>
         </div>
       </div>
     </FadeInTransition>
